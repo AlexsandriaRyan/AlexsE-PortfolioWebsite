c4d603499d25d5618127689101d8f6adcad5c34b
AlexsandriaRyan <w0466134@nscc.ca>
2022-12-18 00:44:08 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function mouseOver1() {
+    document.getElementById("about").src = "./images/Icons/about-col.gif";
+}
+
+function mouseOut1() {
+    document.getElementById("about").src = "./images/Icons/about-bw.jpg";
+}
+
+function mouseOver2() {
+    document.getElementById("projects").src = "./images/Icons/projects-col.gif";
+}
+
+function mouseOut2() {
+    document.getElementById("projects").src = "./images/Icons/projects-bw.jpg";
+}
+
+function mouseOver3() {
+    document.getElementById("skills").src = "./images/Icons/skills-col.gif";
+}
+
+function mouseOut3() {
+    document.getElementById("skills").src = "./images/Icons/skills-bw.jpg";
+}
+
+function mouseOver4() {
+    document.getElementById("contact").src = "./images/Icons/contact-col.gif";
+}
+
+function mouseOut4() {
+    document.getElementById("contact").src = "./images/Icons/contact-bw.jpg";
+}
