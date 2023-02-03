487c9b45dc3a932596cedf3422d85beb5808c7b0
AlexsandriaRyan <w0466134@nscc.ca>
2023-02-03 13:59:56 -0400

    update javascript with onclick events and mobile / desktop compatibility

--- js/script.js
+++ js/script.js
@@ -1,47 +1,79 @@
-function mouseOver1() {
-    document.getElementById("about").src = "./images/Icons/about-col.gif";
-}
+// execute only if on a computer
 
-function mouseOut1() {
-    document.getElementById("about").src = "./images/Icons/about-bw.jpg";
-}
+if (/Android|BlackBerry|iPhone|iPad|iPod|webOS/i.test(navigator.userAgent) === false) {
 
-function mouseOver2() {
-    document.getElementById("projects").src = "./images/Icons/projects-col.gif";
-}
+    function mouseOver1() {
+        document.getElementById("about").src = "./images/Icons/about-col.gif";
+    }
+    
+    function mouseOut1() {
+        document.getElementById("about").src = "./images/Icons/about-bw.jpg";
+    }
+    
+    function mouseOver2() {
+        document.getElementById("projects").src = "./images/Icons/projects-col.gif";
+    }
+    
+    function mouseOut2() {
+        document.getElementById("projects").src = "./images/Icons/projects-bw.jpg";
+    }
+    
+    function mouseOver3() {
+        document.getElementById("skills").src = "./images/Icons/skills-col.gif";
+    }
+    
+    function mouseOut3() {
+        document.getElementById("skills").src = "./images/Icons/skills-bw.jpg";
+    }
+    
+    function mouseOver4() {
+        document.getElementById("contact").src = "./images/Icons/contact-col.gif";
+    }
+    
+    function mouseOut4() {
+        document.getElementById("contact").src = "./images/Icons/contact-bw.jpg";
+    }
+    
+    function mouseOver5() {
+        document.getElementById("website").src = "./images/Icons/website-col.gif";
+    }
+    
+    function mouseOut5() {
+        document.getElementById("website").src = "./images/Icons/website-bw.jpg";
+    }
+    
+    function mouseOver6() {
+        document.getElementById("calculator").src = "./images/Icons/calculator-col.gif";
+    }
+    
+    function mouseOut6() {
+        document.getElementById("calculator").src = "./images/Icons/calculator-bw.jpg";
+    }
 
-function mouseOut2() {
-    document.getElementById("projects").src = "./images/Icons/projects-bw.jpg";
-}
+  } else {
 
-function mouseOver3() {
-    document.getElementById("skills").src = "./images/Icons/skills-col.gif";
-}
-
-function mouseOut3() {
-    document.getElementById("skills").src = "./images/Icons/skills-bw.jpg";
-}
-
-function mouseOver4() {
-    document.getElementById("contact").src = "./images/Icons/contact-col.gif";
-}
-
-function mouseOut4() {
-    document.getElementById("contact").src = "./images/Icons/contact-bw.jpg";
-}
-
-function mouseOver5() {
-    document.getElementById("website").src = "./images/Icons/website-col.gif";
-}
-
-function mouseOut5() {
-    document.getElementById("website").src = "./images/Icons/website-bw.jpg";
-}
-
-function mouseOver6() {
-    document.getElementById("calculator").src = "./images/Icons/calculator-col.gif";
-}
-
-function mouseOut6() {
-    document.getElementById("calculator").src = "./images/Icons/calculator-bw.jpg";
-}
+    function onClick1() {
+        document.getElementById("about").src = "./images/Icons/about-col.jpg";
+    }
+    
+    function onClick2() {
+        document.getElementById("projects").src = "./images/Icons/projects-col.jpg";
+    }
+    
+    function onClick3() {
+        document.getElementById("skills").src = "./images/Icons/skills-col.jpg";
+    }
+    
+    function onClick4() {
+        document.getElementById("contact").src = "./images/Icons/contact-col.jpg";
+    }
+    
+    function onClick5() {
+        document.getElementById("website").src = "./images/Icons/website-col.jpg";
+    }
+    
+    function onClick6() {
+        document.getElementById("calculator").src = "./images/Icons/calculator-col.jpg";
+    }
+    
+  }
